b4cabe571240db79dff644b23b17f117a5cb1b68
yuI4140 <102521443+yuI4140@users.noreply.github.com>
2023-08-08 19:04:20 +0200

    Update build.bat

--- build/build.bat
+++ build/build.bat
@@ -1,2 +1,2 @@
 
-gcc -Wall -Wextra -Werror -L./lib -I./headers -o ./bin/main ./src/main.c -lraylib
+gcc -Wall -Wextra -Werror -L./lib -I./headers -o ./bin/main ./src/main.c -lraylib -lopengl32 -lgdi32 -lwinmm
